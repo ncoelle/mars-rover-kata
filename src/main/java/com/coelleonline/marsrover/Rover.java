581197ed22e4e30ad3d464259006062bc41f20e2
Niels @ home <niels@coelle-online.com>
2017-09-16 05:52:49 +0200

    Direction moved up as independent class

--- src/main/java/com/coelleonline/marsrover/Rover.java
+++ src/main/java/com/coelleonline/marsrover/Rover.java
@@ -15,42 +15,4 @@ class Rover {
 		}
 		return "0:0:" + currentDirection.value();
 	}
-
-	enum Direction {
-		NORTH("N", "W", "E"),
-		EAST("E", "N", "S"),
-		SOUTH("S", "E", "W"),
-		WEST("W", "S", "N");
-
-		private final String value;
-		private final String left;
-		private final String right;
-
-		Direction(String value, String left, String right) {
-			this.value = value;
-			this.left = left;
-			this.right = right;
-		}
-
-		public String value() {
-			return value;
-		}
-
-		public Direction left() {
-			return directionMatching(left);
-		}
-
-		public Direction right() {
-			return directionMatching(right);
-		}
-
-		private Direction directionMatching(String dir) {
-			for (Direction direction : values()) {
-				if (direction.value.equals(dir)) {
-					return direction;
-				}
-			}
-			throw new IllegalStateException("This should never happen: " + this.toString());
-		}
-	}
 }
